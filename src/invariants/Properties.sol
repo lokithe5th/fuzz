56b229c07623728084ea60ba2d6143921e160fec
lokithe5th <lourenslinde@gmail.com>
2024-05-13 13:05:31 +0200

    Update Properties.sol

--- src/invariants/Properties.sol
+++ src/invariants/Properties.sol
@@ -7,16 +7,7 @@ import {Setup, YourContract} from "./Setup.sol";
 
 abstract contract Properties is Setup, Asserts {
 
-    function invariant_Accrual(address user, uint256 time) internal view returns (bool) {
-        (uint256 cap, uint256 last, address token) = yourContract.streamedBuilders(user);
-        uint256 actualAccrual = yourContract.unlockedBuilderAmount(user);
-
-        if (time > last + 2592000) {
-            return actualAccrual == cap;
-        }
-
-        // Can abstract this to a helper function
-        uint256 expectedAccrual = cap * (block.timestamp - last) / 2592000;
-        return expectedAccrual == actualAccrual;
+    function crytic_canary() internal view returns (bool) {
+        return false;
     }
 }
